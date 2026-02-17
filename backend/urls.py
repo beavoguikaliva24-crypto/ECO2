bf28563f3c2af9ef88c9beb20e2da44452be4ee4
Kaliva Beavogui <beavogui.kaliva24@gmail.com>
2026-02-17 20:04:47 +0000

    Installation de Django et configuration initiale de la DB

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # On préfixe toutes les routes de l'application par 'api/'
+    path('api/', include('ecorys.urls')), 
 ]
+
+# Très important pour afficher les photos des élèves en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
